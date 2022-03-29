ff804a943c2a49842a9304599c2fbb58ffb50e54
Gopi <gopi.sv@verticurl.com>
2022-03-30 04:55:41 +0530

    Added web api project

---
+++ src/Feature/Patients/Website/Controllers/PrescriptionCreationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Website.Controllers
+{
+    public class PrescriptionCreationController : ApiController
+    {
+        public IHttpActionResult CreatePrescription()
+        {
+            return Json("test");
+        }
+    }
+}
--- src/Project/Publishes/Website/Properties/AssemblyInfo.cs
+++ src/Project/Publishes/Website/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Molina.Feature.Patients")]
+[assembly: AssemblyTitle("Molina.Project.Publishes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Molina.Feature.Patients")]
+[assembly: AssemblyProduct("Molina.Project.Publishes")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6bdd3082-933b-46fe-a18e-bca2ba7c430e")]
+[assembly: Guid("0fa15fd0-0b07-4b18-9591-c53783f193d4")]
 
 // Version information for an assembly consists of the following four values:
 //
